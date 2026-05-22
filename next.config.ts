b222e16f7d91c96b62b7ce1298b739333cf0ea59
jj159207-driod <jj159207@gmail.com>
2026-05-22 20:33:09 +0800

    disable cache

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+﻿import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  headers: async () => [
+    {
+      source: "/(.*)",
+      headers: [
+        { key: "Cache-Control", value: "no-store, max-age=0" },
+      ],
+    },
+  ],
 };
 
 export default nextConfig;
