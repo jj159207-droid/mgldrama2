289949f8f4262887cd469428c06bb2f4af8a8146
jj159207-driod <jj159207@gmail.com>
2026-05-22 11:45:20 +0800

    fix cyrillic font and lang

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,20 @@
-import type { Metadata } from "next";
+﻿import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
-  subsets: ["latin"],
+  subsets: ["latin", "cyrillic"],
 });
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
-  subsets: ["latin"],
+  subsets: ["latin", "cyrillic"],
 });
 
 export const metadata: Metadata = {
-  title: "кино үзэх самбар",
-  description: "кино үзэх самбар",
+  title: "Кино үзэх самбар",
+  description: "Кино үзэх самбар",
 };
 
 export default function RootLayout({
@@ -24,10 +24,11 @@ export default function RootLayout({
 }>) {
   return (
     <html
-      lang="en"
+      lang="mn"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
 }
+
