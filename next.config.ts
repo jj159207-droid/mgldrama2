326c0cf9dfb229d09e691c219c431dcc2b706009
jj159207-driod <jj159207@gmail.com>
2026-05-23 15:32:48 +0800

    add next-pwa

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,13 @@
-﻿import type { NextConfig } from "next";
+﻿import withPWA from "next-pwa";
 
-const nextConfig: NextConfig = {
+const pwaConfig = withPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
+
+export default pwaConfig({
   headers: async () => [
     {
       source: "/(.*)",
@@ -9,6 +16,4 @@ const nextConfig: NextConfig = {
       ],
     },
   ],
-};
-
-export default nextConfig;
+});
