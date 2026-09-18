87348a04c31234b0e605bcf8962072fbd4deeca8
jj159207-droid <jj159207@gmail.com>
2026-09-18 19:30:25 +0800

    Add short movie URL route

---
+++ app/[filmId]/page.tsx
@@ -0,0 +1,11 @@
+import { notFound, redirect } from "next/navigation";
+
+export default async function FilmShortLinkPage({
+  params,
+}: {
+  params: Promise<{ filmId: string }>;
+}) {
+  const { filmId } = await params;
+  if (!/^[1-9]\d*$/.test(filmId)) notFound();
+  redirect(`/?film=${filmId}`);
+}
