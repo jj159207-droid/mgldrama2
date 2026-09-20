a378bddf87836886d8ef11ca63fceab2e89215cd
jj159207-droid <jj159207@gmail.com>
2026-09-20 17:29:25 +0800

    Brand КИНОЧИД route

--- app/kinochid/page.tsx
+++ app/kinochid/page.tsx
@@ -1,2 +1,9 @@
+import type { Metadata } from "next";
 import Home from "../page";
+
+export const metadata: Metadata = {
+  title: "КИНОЧИД",
+  description: "КИНОЧИД кино сайт",
+};
+
 export default Home;
