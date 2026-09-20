e405aa8455c9ae7d17ed3354ccbb1430d2a1568a
jj159207-droid <jj159207@gmail.com>
2026-09-20 17:29:34 +0800

    Brand FIRE route

--- app/fire/page.tsx
+++ app/fire/page.tsx
@@ -1,2 +1,9 @@
+import type { Metadata } from "next";
 import Home from "../page";
+
+export const metadata: Metadata = {
+  title: "FIRE",
+  description: "FIRE кино сайт",
+};
+
 export default Home;
