a56cf0fbf794166f1674768bbf1665a51b3b67c7
jj159207-droid <jj159207@gmail.com>
2026-09-20 17:29:15 +0800

    Brand КИНО ДРАМА route

--- app/kino-drama/page.tsx
+++ app/kino-drama/page.tsx
@@ -1,2 +1,9 @@
+import type { Metadata } from "next";
 import Home from "../page";
+
+export const metadata: Metadata = {
+  title: "КИНО ДРАМА",
+  description: "КИНО ДРАМА кино сайт",
+};
+
 export default Home;
